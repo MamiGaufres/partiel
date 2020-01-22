ed569e6e23294d6b34a7a19ddcf43e9d1f294206
MamiGaufres <mogofranca@live.fr>
2020-01-22 13:15:42 +0100

    initial

--- src/data/Etudiant.java
+++ src/data/Etudiant.java
@@ -56,4 +56,88 @@ public class Etudiant extends Personne {
 		return true;
 	}
 	
+	public static void supprimerEtudiant(String id) {
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn = DriverManager.getConnection(db_host, db_user, db_passwd);
+			st = cn.createStatement();
+			String sql = "DELETE FROM etudiants WHERE etudiant_id = '"+id+"'";
+			st.executeUpdate(sql);
+			System.out.println("");
+ 		    System.out.print("Suppression de l'étudiant #"+id+".");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e ) {
+			e.printStackTrace();
+		} finally {
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void majEtudiant(String id, String etudiant_nom, String etudiant_prenom, String etudiant_mail, String etudiant_adresse, String etudiant_tel, String etudiant_naissance) {
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn = DriverManager.getConnection(db_host, db_user, db_passwd);
+			st = cn.createStatement();
+			String sql = "UPDATE etudiants SET etudiant_nom = '"+etudiant_nom+"',"
+					+ " etudiant_prenom = '"+etudiant_prenom+"',"
+					+ " etudiant_mail = '"+etudiant_mail+"',"
+					+ " etudiant_tel = '"+etudiant_tel+"',"
+					+ " etudiant_naissance = '"+etudiant_naissance+"',"
+					+ " etudiant_adresse = '"+etudiant_adresse+"' WHERE etudiant_id = '"+id+"'";
+			System.out.println(sql);
+			st.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e ) {
+			e.printStackTrace();
+		} finally {
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void listerEtudiants() {
+		
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn = DriverManager.getConnection(db_host, db_user, db_passwd);
+			st = cn.createStatement();
+			String sql = "SELECT * FROM etudiants";
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				System.out.println("#"+rs.getString("etudiant_id") + " - " + rs.getString("etudiant_prenom") + " " + rs.getString("etudiant_nom") + "(" +rs.getString("etudiant_mail")+")");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e ) {
+			e.printStackTrace();
+		} finally {
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -19,7 +19,7 @@ public class Main {
            
            System.out.print("Adresse email : ");
            String login = br.readLine();
-           System.out.println("");
+           System.out.print("");
            
            System.out.print("Mot de passe : ");
            String mdp = br.readLine();
@@ -27,10 +27,16 @@ public class Main {
 			
            if(Enseignant.authentification(login, mdp)) {
         	   System.out.println("1 - Créér un étudiant ");
+        	   System.out.println("2 - Lister les étudiants ");
+        	   System.out.println("3 - Supprimer un étudiant (via code) ");
+        	   System.out.println("4 - Supprimer un étudiant (via code) ");
+        	   System.out.println("R - Redémarrer ");
         	   System.out.println("Q - Quitter ");
-        	   
+        	   System.out.println("");
+        	   System.out.print("> ");
         	   String value = br.readLine();
         	   if(value.equals("1")) {
+        		   System.out.println("");
         		   System.out.println("Nom etudiant : ");
                    String nom = br.readLine();
                    System.out.println("Préom etudiant : ");
@@ -44,13 +50,59 @@ public class Main {
                    System.out.println("Date de naissance etudiant : ");
                    String dateNaissance = br.readLine();
                    Etudiant.creer(nom, prenom, mail, adresse, telephone, dateNaissance);
+        		   System.out.println("");
+        		   System.out.println("L'étudiant a été ajouté au système.");
+        		   System.exit(0);
+        	   }
+        	   
+        	   if(value.equals("2")) {
+        		   System.out.println("");
+                   Etudiant.listerEtudiants();
+        		   System.exit(0);
+        	   }
+        	   
+        	   if(value.equals("3")) {
+        		   System.out.println("");
+        		   System.out.print("Code de l'étudiant : ");
+        		   String id = br.readLine();
+        		   System.out.println("");
+                   Etudiant.supprimerEtudiant(id);;
+        		   System.exit(0);
+        	   }
+        	   
+        	   if(value.equals("4")) {
+        		   System.out.println("");
+        		   System.out.print("Code de l'étudiant : ");
+        		   String id = br.readLine();
+        		   System.out.println("");
+        		   System.out.println("Nom etudiant : ");
+                   String nom = br.readLine();
+                   System.out.println("Préom etudiant : ");
+                   String prenom = br.readLine();
+                   System.out.println("Mail etudiant : ");
+                   String mail = br.readLine();
+                   System.out.println("Adresse etudiant : ");
+                   String adresse = br.readLine();
+                   System.out.println("Téléphone etudiant : ");
+                   String telephone = br.readLine();
+                   System.out.println("Date de naissance etudiant : ");
+                   String dateNaissance = br.readLine();
+                   Etudiant.majEtudiant(id, nom, prenom, mail, adresse, telephone, dateNaissance);
+        		   System.exit(0);
         	   }
         	   
         	   if(value.equals("Q")) {
         		   System.out.println("");
-        		   System.out.println("");
         		   System.out.println("A bientôt !");
         		   System.exit(0);
+        	   }
+        	   
+        	   if(value.equals("R")) {
+        		   System.out.println("");
+        		   System.out.println("Redémarrage en cours ...");
+        		   System.out.println("");
+        		   System.out.println("");
+        		   Main.main(args);
                    
         	   }
         	   
